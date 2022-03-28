5f58c8c8e27b0a9eee1136fede45d2ecdccb2bf0
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-03-28 19:09:54 +0300

    CRUD: Update and Delete

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const createFileStatus = await file.create('users', 'petras.json', petras);
 console.log(createFileStatus);
 const readFileStatus1 = await file.read('users', 'petras.json');
 console.log(readFileStatus1);
-file.update();
+const updateFileStatus1 = await file.update('users', 'petras.json', {
+  name: 'Petras',
+  age: 99,
+});
+console.log(updateFileStatus1);
 const readFileStatus2 = await file.read('users', 'petras.json');
 console.log(readFileStatus2);
-file.delete();
+const deleteFileStatus1 = await file.delete('users', 'petras.json');
+console.log(deleteFileStatus1);
 const readFileStatus3 = await file.read('users', 'petras.json');
 console.log(readFileStatus3);
